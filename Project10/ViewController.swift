6c43c4dc082e5e082e768363dfa8a7fd0686fe44
buuraak77 <brkylmz8@hotmail.com>
2022-07-08 15:42:46 +0300

    Initial Commit

---
+++ Project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Burak Yılmaz on 8.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
